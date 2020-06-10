f1584bdc6ee8e8dfd652cbd3c17b47a8b7e004d3
timoleonn <tim.charilaou@gmail.com>
2020-06-11 01:08:56 +0300

    Fixed script

--- script.py
+++ script.py
@@ -2,7 +2,9 @@ from datetime import datetime
 
 # datetime object containing current date and time
 now = datetime.now()
+dt_string = now.strftime("%d-%m-%Y_%H-%M-%S")
 
-filename = "myfile_{}".format(now)
+filename = "myfile_{}.txt".format(dt_string)
+
 f = open(filename, "x")
 f.close()
